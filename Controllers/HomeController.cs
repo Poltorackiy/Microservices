7d669d53681d44b121b0735bc9dd172da47d37cd
Konstantin <k.poltarackiy@gmail.com>
2018-06-12 14:58:49 +0700

    Добавлен функционал создания / удаления депозитов

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microservices.Deposits.Models;
-using MySql.Data.MySqlClient;
 using Microsoft.Extensions.Configuration;
 using System.IO;
+using System.Xml.Linq;
 
 namespace Microservices.Deposits.Controllers
 {
@@ -15,8 +15,38 @@ namespace Microservices.Deposits.Controllers
     {
         public IActionResult Index()
         {
-            string connectionString = GetMysqlConnectionString();
-            return View();
+            return View(GetDeposits());
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, string interestRate, string capitalization)
+        {
+            var deposits = GetDeposits();
+            Deposit deposit = new Deposit();
+            deposit.Name = name;
+            // Проверка на стороне клиента. 
+            // В идеале сделать проверку на стороне сервера и выдать сообщение об ошибке
+            deposit.InterestRate = Convert.ToDecimal(interestRate.Replace(".", ",")); 
+            deposit.Capitalization = (capitalization == "on") ? true : false;
+            deposit.Guid = Guid.NewGuid();
+            deposits.Add(deposit);
+            SaveDeposits(deposits);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string guid)
+        {
+            var deposits = GetDeposits();
+            var deposit = deposits.Where(d => d.Guid == Guid.Parse(guid)).First();
+            if (deposit != null)
+            {
+                deposits.Remove(deposit);
+                SaveDeposits(deposits);
+            }
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult About()
@@ -38,20 +68,78 @@ namespace Microservices.Deposits.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        private string GetMysqlConnectionString()
+        private List<Deposit> GetDeposits()
         {
-            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
+            Stream stream;
+            XDocument document = null;
+            List<Deposit> deposits = new List<Deposit>();
 
-            IConfiguration config = builder.Build();
-            sb.Server = config.GetValue<string>("MySQL:Host");
-            sb.Port = config.GetValue<uint>("MySQL:Port");
-            sb.UserID = config.GetValue<string>("MySQL:User");
-            sb.Password = config.GetValue<string>("MySQL:Password");
+            try
+            {
+                using (stream = System.IO.File.Open("Deposits.xml", FileMode.Open))
+                {
+                    document = new XDocument();
+                    document = XDocument.Load(stream);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                using (stream = System.IO.File.Create("Deposits.xml")) { }
+                return new List<Deposit>();
+            }
+            catch (Exception)
+            {
+                return new List<Deposit>();
+            }
 
-            return sb.ToString();
+            deposits = document.Root.Elements("deposit")
+                .Select(dep => new Deposit
+                {
+                    Name = dep.Element("name").Value,
+                    InterestRate = Convert.ToDecimal(dep.Element("interest_rate").Value.Replace(".", ",")),
+                    Capitalization = Convert.ToBoolean(dep.Element("capitalization").Value),
+                    Guid = Guid.Parse(dep.Element("guid").Value)
+                }).ToList();
+                 
+                
+
+
+
+            /*foreach (var element in document.XPathSelectElements("deposits/deposit"))
+            {
+                Deposit deposit = new Deposit();
+                var xName = element.XPathSelectElement("/deposit/name");
+                deposit.Name = element.XPathSelectElement("/deposit/name").Value;
+                deposit.InterestRate = Convert.ToDecimal(element.XPathSelectElement("/deposit/interest_rate").Value.Replace(".", ","));
+                if (element.XPathSelectElement("/deposit/capitalization").Value == "True")
+                    deposit.Capitalization = true;
+                else
+                    deposit.Capitalization = false;
+
+                deposits.Add(deposit);
+            }*/
+
+            return deposits;
         }
+
+        private void SaveDeposits(List<Deposit> deposits)
+        {
+            XDocument document = new XDocument();
+            XElement root = new XElement("deposits");
+            document.AddFirst(root);
+            foreach (var deposit in deposits)
+            {
+                XElement xDeposit = new XElement("deposit",
+                    new XElement("guid", deposit.Guid.ToString()),
+                    new XElement("name", deposit.Name),
+                    new XElement("interest_rate", deposit.InterestRate),
+                    new XElement("capitalization", deposit.Capitalization.ToString()));
+
+                root.Add(xDeposit);
+            }
+
+            document.Save("Deposits.xml");
+        }
+
     }
 }
---
+++ Models/Deposit.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservices.Deposits.Models
+{
+    public class Deposit
+    {
+        private decimal _interestRate;
+
+        /// <summary>
+        /// Гуид депозита
+        /// </summary>
+        public Guid Guid { get; set; }
+
+        /// <summary>
+        /// Название вклада
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Процентная ставка
+        /// </summary>
+        public decimal InterestRate
+        {
+            get
+            {
+                return _interestRate;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Процентая ставка не может быть отрицательной");
+
+                _interestRate = value;
+            }
+        }
+
+        /// <summary>
+        /// Капитализация процентов
+        /// </summary>
+        public bool Capitalization { get; set; }
+
+        /// <summary>
+        /// Количество средств после закрытия вклада
+        /// </summary>
+        /// <param name="StartCapital">Первоначальная сумма вложения</param>
+        /// <param name="Months">Количество месяцев</param>
+        /// <returns></returns>
+        public decimal TotalAmount(decimal StartCapital, int Months)
+        {
+            decimal result = StartCapital;
+            for (int i = 0; i < Months; i++)
+            {
+                if (Capitalization == true)
+                    result = result + result * InterestRate / 12;
+                else
+                    result = result + StartCapital * InterestRate / 12;
+            }
+            return result;
+        }
+
+    }
+}
